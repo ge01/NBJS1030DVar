0e74591c7b22df51da9d6715d081e31084b83bcb
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-30 00:24:55 -0500

    Different Types of Variables

---
+++ app.js
@@ -0,0 +1,14 @@
+/****************************************************
+*           Different Types of Variables            *
+****************************************************/
+var brk = "<br>";
+var tuna = "john said, \"Boy do i love bessie\" <br>";
+document.write(tuna);
+
+var cat = true;
+document.write(cat);
+document.write(brk);
+
+var noVal = null;
+document.write(noVal);
+document.write(brk);
